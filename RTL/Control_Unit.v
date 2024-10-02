19623635c9d3ac125fc143d55fc70a5563e950de
Vishvas2129 <vishvasvalupadasu@gmail.com>
2024-10-02 20:21:01 +0530

    Add files via upload

---
+++ RTL/Control_Unit.v
@@ -0,0 +1,34 @@
+`include "ALU_Dec.v"
+`include "Main_Dec.v"
+
+module Control_Unit(Op,RegWrite,ImmSrc,ALUSrc,MemWrite,ResultSrc,Branch,funct3,funct7,ALUControl);
+
+    input [6:0]Op,funct7;
+    input [2:0]funct3;
+    output RegWrite,ALUSrc,MemWrite,ResultSrc,Branch;
+    output [1:0]ImmSrc;
+    output [2:0]ALUControl;
+
+    wire [1:0]ALUOp;
+
+    Main_Dec Main_Dec(
+                .Op(Op),
+                .RegWrite(RegWrite),
+                .ImmSrc(ImmSrc),
+                .MemWrite(MemWrite),
+                .ResultSrc(ResultSrc),
+                .Branch(Branch),
+                .ALUSrc(ALUSrc),
+                .ALUOp(ALUOp)
+    );
+
+    ALU_Dec ALU_Dec(
+                            .ALUOp(ALUOp),
+                            .funct3(funct3),
+                            .funct7(funct7),
+                            .op(Op),
+                            .ALUControl(ALUControl)
+    );
+
+
+endmodule
